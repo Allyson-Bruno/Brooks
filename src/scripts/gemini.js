9745118baa8cee0ad11be0bd73f62642f0dbbb9b
Allyson Bruno <allbruno2622@gmail.com>
2026-06-27 20:54:39 -0300

    Início do projeto

---
+++ src/scripts/gemini.js
@@ -0,0 +1,56 @@
+
+const API_KEY = "SUA_CHAVE_DE_API";
+
+const MODELO = "gemini-2.5-flash";
+
+const URL = `https://generativelanguage.googleapis.com/v1beta/models/${MODELO}:generateContent`;
+
+async function chamarGemini(prompt) {
+
+const corpoRequisicao = {
+
+contents: [{
+
+parts: [{text: prompt }]
+
+}]
+
+};
+
+try {
+
+const response = await fetch(URL, {
+
+method: "POST",
+
+headers: {
+
+"Content-Type": "application/json",
+
+"x-goog-api-key": API_KEY
+
+},
+
+body: JSON.stringify(corpoRequisicao)
+
+});
+
+if (!response.ok) {
+
+throw new Error(`Erro na API: ${response.status} - ${response.statusText}`);
+
+}
+
+const dados = await response.json();
+
+return dados.candidates[0].content.parts[0].text;
+
+} catch (erro) {
+
+console.error("Algo deu errado na requisição:", erro);
+
+return null;
+
+}
+
+};
